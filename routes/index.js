4501bfe8c9133e6dd8ced10b3fc9e5c04e3917c1
Carol Alonso <c.alonsolp@gmail.com>
2020-07-02 18:22:48 +0200

    task: movies

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+/* GET movies */
+router.get('/movies', (req, res, next) => {
+	Movie.find()
+		.then((dbMovies) => {
+			res.render('movies', { movies: dbMovies })
+		})
+		.catch((e) => next(e));
+})
 module.exports = router;
